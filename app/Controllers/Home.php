1a02f1109ddd19e7b391c49f6188d1f76aba6216
SebaFrei <sebastianfreiberger03@gmail.com>
2022-07-22 17:46:31 -0300

    subo pagina codeigniter4

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        echo view("includes/header");
+        echo view("index");
+        echo view("includes/footer");
+
+    }
+}
